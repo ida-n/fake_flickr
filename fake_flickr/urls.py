b62834d14dfac067fb25ca562bd61d4a303cf526
Ida N <ida.nakhostin@gmail.com>
2016-01-16 19:39:35 -0800

    Fixed urls for 1.9

--- fake_flickr/urls.py
+++ fake_flickr/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = [
+# Examples:
     # url(r'^$', 'fake_flickr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+    # url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
